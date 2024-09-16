d51ef0c496463c5204c438bb4f353c9e3d1ded13
SleepingChameleon <rlarombe10@gmail.com>
2024-09-17 02:10:43 +0800

    feat: product crud done and tested in postman

--- src/api/inventory/inventory.route.ts
+++ src/api/inventory/inventory.route.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import productRoute from './inventory/product/product.route';
+import log from '../../../lib/logger';
 
 // import supplierRoute from './inventory/supplier/supplier.route';
 // import itemRoute from './inventory/inventory.route';
@@ -9,5 +11,7 @@ const imsRoute = Router({ mergeParams: true });
 // imsRoute.use('/supplier', supplierRoute);
 // imsRoute.use('/inventory', itemRoute);
 // imsRoute.use('/tag-items', tagitemRoute);
+imsRoute.use('/product', productRoute);
+log.info('ROUTE /product set');
 
 export default imsRoute;
---
+++ src/api/inventory/inventory/product/product.middleware.ts
@@ -0,0 +1,37 @@
+import { eq, and, isNull } from 'drizzle-orm';
+import { NextFunction, Request, Response } from 'express';
+
+import log from '../../../../../lib/logger';
+import { db } from '../../../../../mysql/mysql.pool';
+import { product } from '../../../../../drizzle/drizzle.schema';
+
+// There's a globally used
+// middleware like error handling and schema validation
+
+export async function validateProductID(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { product_id } = req.params;
+
+  try {
+    const Product = await db
+      .select()
+      .from(product)
+      .where(
+        and(
+          eq(product.product_id, Number(product_id)),
+          isNull(product.deleted_at),
+        ),
+      );
+    console.log(Product);
+    if (!Product[0]) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    next();
+  } catch (error) {
+    log.error(error);
+    res.status(500).json({ message: 'Internal Server Error ' });
+  }
+}
