914b62a90c1a533878dbcbf9d2d00a29b8558071
SleepingChameleon <rlarombe10@gmail.com>
2024-09-22 03:21:05 +0800

    feat: channel crud done, and tested in postman

--- src/api/cms/cms.route.ts
+++ src/api/cms/cms.route.ts
@@ -3,6 +3,7 @@ import log from '../../../lib/logger';
 import customerRoute from './customer/customer.route';
 import inquiryRoute from './inquiry/inquiry.route';
 import messageRoute from './message/message.route';
+import channelRoute from './channel/channel.route';
 
 const cmsRoute = Router({ mergeParams: true });
 
@@ -14,5 +15,8 @@ log.info('ROUTE inquiry set');
 
 cmsRoute.use('/message', messageRoute);
 log.info('ROUTE message set');
+
+cmsRoute.use('/channel', channelRoute);
+log.info('ROUTE channel set');
 
 export default cmsRoute;
