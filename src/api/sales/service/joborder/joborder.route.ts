04f31e35029b97a191424f9da0bd9ffbfcf79750
ajtollo <khesir2002@gmail.com>
2024-10-18 21:41:55 +0800

    refactor: change api route for new modules

--- src/api/sales/service/joborder/joborder.route.ts
+++ src/api/sales/service/joborder/joborder.route.ts
@@ -6,6 +6,8 @@ import { JobOrderController } from './joborder.controller';
 import { validateJobOrderID } from './joborder.middleware';
 import { CreateJobOrder, UpdateJobOrder } from './joborder.model';
 import reportsRoute from './reports/reports.route';
+import assignedEmployeeRoute from './assignedemployees/assignedemployees.route';
+import remarkTicketsRoute from './remarktickets/remarkticket.route';
 
 const joborderRoute = Router({ mergeParams: true });
 const joborderController = new JobOrderController(db);
@@ -44,7 +46,21 @@ joborderRoute.delete(
 );
 log.info('DELETE /joborder/:job_order_id set');
 
-joborderRoute.use('/:job_order_id/reports', reportsRoute);
+joborderRoute.use('/:job_order_id/reports', validateJobOrderID, reportsRoute);
+log.info('ROUTE Job order reports set');
+
+joborderRoute.use(
+  '/:job_order_id/assigned-employee',
+  validateJobOrderID,
+  assignedEmployeeRoute,
+);
+log.info('ROUTE Job order assigned employee set');
+
+joborderRoute.use(
+  '/:job_order_id/remark-tickets',
+  validateJobOrderID,
+  remarkTicketsRoute,
+);
 log.info('ROUTE Job order reports set');
 
 export default joborderRoute;
