ad807875f8ab28bd4b3c21407d07a145bbdf0405
SleepingChameleon <rlarombe10@gmail.com>
2024-10-23 22:01:03 +0800

    feat: job order types done and tested in postman

--- src/api/sales/sales.route.ts
+++ src/api/sales/sales.route.ts
@@ -3,6 +3,7 @@ import salesRoute from './sales/sales.route';
 import log from '@/lib/logger';
 import salesitemRoute from './salesItem/salesItem.route';
 import serviceRoute from './service/serviceses.route';
+import jobordertypesRoute from './jobordertypes/jobordertypes.route';
 
 const smsRoute = Router({ mergeParams: true });
 
@@ -15,4 +16,6 @@ log.info('ROUTE salesitem set');
 smsRoute.use('/service', serviceRoute);
 log.info('ROUTE service set');
 
+smsRoute.use('/jobordertypes', jobordertypesRoute);
+log.info('ROUTE job order types');
 export default smsRoute;
